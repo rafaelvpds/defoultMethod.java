9db1b8e19482ba9ce058d7fe39d63f830610b7d2
rafaelvpds <33781893+rafaelvpds@users.noreply.github.com>
2023-06-21 16:00:08 -0300

    Sem interface

---
+++ src/service/BrasilInterestService.java
@@ -0,0 +1,27 @@
+package service;
+
+import java.security.InvalidParameterException;
+
+public class BrasilInterestService {
+	private Double interestRate;
+
+	public BrasilInterestService(Double interestRate) {
+
+		this.interestRate = interestRate;
+	}
+
+	public Double getInterestRate() {
+		return interestRate;
+	}
+
+	public void setInterestRate(Double interestRate) {
+		this.interestRate = interestRate;
+	}
+
+	public double payment(double amount, int month) {
+		if (month < 1) {
+			throw new InvalidParameterException("Months must greater than zero");
+		}
+		return amount * Math.pow(1.0 + interestRate / 100, month);
+	}
+}
